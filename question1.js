8470f12012bddbc504850a5464e70c499ff2dccd
Rahul Sahani <rahul@wingmanpartners.com>
2024-10-30 13:44:20 +0530

    Update question1.js

--- question1.js
+++ question1.js
@@ -4,4 +4,4 @@ const year = date.getFullYear();
 const month = date.getMonth();
 const day = date.getDate();
 
-console.log(day+"/"+month+"/"+year);
+console.log(`${month}-${day}-${year} , `+month+"/"+day+"/"+year);
