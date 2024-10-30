7de916e7b0a8aabb5cddfd5f52d03e2c2c0c9d72
Rahul Sahani <rahul@wingmanpartners.com>
2024-10-30 15:04:00 +0530

    Update question5.js

--- question5.js
+++ question5.js
@@ -3,11 +3,12 @@ var output= document.querySelector("#output");
 function taskinput() {
     const number = Math.floor(Math.random() * 10 + 1);  //generate random no. between 1 to 10
     console.log(number);
+    output.innerHTML=""
 
     const userinput = document.getElementById("userinput").value;
 
     // Handling user input
-    if (userinput == null || userinput < 1 || userinput > 10) {alert("Not match");
+    if (userinput == null || userinput < 1 || userinput > 10) {;
       alert("invalid input please enter a number between 1 to 10 ");
     } else displaymessage(userinput ,number);
   }
@@ -15,8 +16,10 @@ function taskinput() {
   function displaymessage(userinput,number) {
     if (number == userinput) {
       console.log("Good Work");
-      output.innerHTML="Good Work" 
+      output.innerHTML="Good Work";
     } 
-    else console.log("Not Matched");
-    output.innerHTML="Not Matched"
+    else 
+    {console.log("Not Matched");
+    output.innerHTML="Not Matched";
+  }
   }
